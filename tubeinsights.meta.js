f94b49979acdfa364d2a14f689b26b60b5714418
afkarxyz <muhammadzamzamafkar@gmail.com>
2025-11-07 07:21:09 +0700

    .

---
+++ tubeinsights.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         TubeInsights
+// @namespace    https://github.com/exyezed/tube-insights/
+// @version      1.0.0-beta.1
+// @author       exyezed
+// @description  youtube metadata powered by InnerTube (youtubei)
+// @license      MIT
+// @supportURL   https://github.com/exyezed/tube-insights/issues
+// @updateURL    https://github.com/exyezed/tube-insights/releases/latest/download/tubeinsights.meta.js
+// @downloadURL  https://github.com/exyezed/tube-insights/releases/latest/download/tubeinsights.user.js
+// ==/UserScript==
