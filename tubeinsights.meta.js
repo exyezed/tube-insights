ead987dca35e03e5e52aad5072c5ac9958e5f2ce
afkarxyz <muhammadzamzamafkar@gmail.com>
2025-11-08 15:03:35 +0700

    v1.0.1

--- tubeinsights.meta.js
+++ tubeinsights.meta.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         TubeInsights
-// @namespace    https://github.com/exyezed/tube-insights/
-// @version      1.0.0
+// @namespace    https://github.com/exyezed/tube-insights
+// @version      1.0.1
 // @author       exyezed
-// @description  youtube metadata powered by InnerTube (youtubei)
+// @description  A feature-rich and high-performance YouTube userscript, built on the InnerTube API — delivering advanced analytics, live stats, smart bookmarking, and seamless video/audio downloading without leaving YouTube.
 // @license      MIT
 // @supportURL   https://github.com/exyezed/tube-insights/issues
 // @updateURL    https://github.com/exyezed/tube-insights/releases/latest/download/tubeinsights.meta.js
